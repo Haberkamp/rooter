5df4f7eddc045b4ded3f9fca4edcb4904a708cd9
Nils Haberkamp <haberkamp.n@gmail.com>
2026-09-19 14:30:34 +0200

    Add route parameter constraints (#9)

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod config;
 mod nav_link;
 mod router;
 
-pub use config::{GuardResult, PageFactory, RouteContext, RouterConfig};
+pub use config::{GuardResult, PageFactory, ParamConstraint, RouteContext, RouterConfig};
 #[doc(hidden)]
 pub use config::{WithAppContext, WithRoute, WithRouteContext, WithoutContext};
 pub use nav_link::NavLink;
